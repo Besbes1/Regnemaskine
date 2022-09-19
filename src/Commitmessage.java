ecf05bedbd2b8f04bf2dc7b971fe523496a452bc
besbes <s224525@student.dtu.dk>
2022-09-19 13:24:28 +0200

    4. commit

---
+++ src/Commitmessage.java
@@ -0,0 +1,7 @@
+public class Commitmessage {
+    public static void main(String[] args) {
+        1
+                2
+
+    }
+}
---
+++ src/LinjePunkt.java
@@ -0,0 +1,16 @@
+import java.math.MathContext;
+
+public class LinjePunkt {
+    public static void main(String[] args) {
+        double a;
+        double b;
+        double x0;
+        double y0;
+
+        // definer formel
+
+
+
+
+    }
+}
