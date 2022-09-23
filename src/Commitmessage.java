a362042b624b97d9b6ce189dc85ba61d6de03ec2
besbes <s224525@student.dtu.dk>
2022-09-23 15:13:50 +0200

    4. commit

--- src/Commitmessage.java
+++ src/Commitmessage.java
@@ -1,7 +1,6 @@
 public class Commitmessage {
     public static void main(String[] args) {
-        1
-                2
+
 
     }
 }
--- src/LinjePunkt.java
+++ src/LinjePunkt.java
@@ -1,13 +1,19 @@
 import java.math.MathContext;
+import java.util.Scanner;
 
 public class LinjePunkt {
     public static void main(String[] args) {
-        double a;
-        double b;
-        double x0;
-        double y0;
+        Scanner input = new Scanner(System.in);
 
-        // definer formel
+        int firstNumber = Integer.parseInt(input.next());
+        int secondNumber = Integer.parseInt(input.next());
+        int thirdNumber = Integer.parseInt(input.next());
+        int forthNumber = Integer.parseInt(input.next());
+
+        System.out.println("enter a,b,x0,y0");
+
+// define linje y=ax+b
+                System.out.println();
 
 
 
